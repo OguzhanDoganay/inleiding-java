b92e2ce7835dcaa0ea4dc14a0202861fb7026132
oguzh <oguzhandoganay@hotmail.nl>
2020-11-09 14:33:53 +0100

    nog niet klaat hfd 5

---
+++ src/Hfd5.java
@@ -0,0 +1,41 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Hfd5 extends Applet {
+
+    public void init() {
+    setBackground(Color.WHITE);
+
+
+
+    }
+
+
+
+
+
+
+    public void paint(Graphics g) {
+g.drawString("lijn",90, 60);
+g.drawLine(50, 40, 150, 40);
+g.drawRect(50, 80, 100, 40);
+g.drawString("Rechthoek", 75, 140 );
+g.setColor(Color.magenta);
+g.fillRect(175, 80, 100, 40);
+g.setColor(Color.BLACK);
+g.drawString("Gevulde rechthoek", 175, 140);
+g.drawOval(175, 80, 100, 40);
+g.setColor(Color.magenta);
+g.fillArc(300, 80, 100, 40, 0, 45);
+g.setColor(Color.BLACK);
+g.drawOval(300, 80, 100, 40);
+g.drawString("Fillarc gebruikt", 300, 140);
+g.drawRoundRect(50, 160, 100, 40, 10, 10);
+
+
+
+
+
+
+    }
+}
--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -1,11 +1,32 @@
 package h01;
 
+import java.applet.Applet;
+import java.awt.*;
+
 /**
  * Created by piet on 31-08-16.
  */
-public class HalloRocDev {
 
-    public static void main(String[] args) {
-        System.out.println("Hallo oguzhan doganay!");
+public class HalloRocDev extends Applet {
+
+    public void init() {
+        setBackground(Color.WHITE);
+
+
+
+    }
+
+
+
+
+
+
+    public void paint(Graphics g) {
+        g.drawString("lijn",50, 50);
+        g.drawLine(50, 40, 100, 40);
+
+
+
+
     }
 }
