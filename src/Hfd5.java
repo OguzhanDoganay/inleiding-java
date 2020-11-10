24ac443e7017f7f048ac7fa448bb748437f1778b
oguzh <oguzhandoganay@hotmail.nl>
2020-11-10 11:11:57 +0100

    eindopdracht hfd 4

--- src/Hfd5.java
+++ src/Hfd5.java
@@ -30,9 +30,14 @@ g.fillArc(300, 80, 100, 40, 0, 45);
 g.setColor(Color.BLACK);
 g.drawOval(300, 80, 100, 40);
 g.drawString("Fillarc gebruikt", 300, 140);
-g.drawRoundRect(50, 160, 100, 40, 10, 10);
-
-
+g.drawRoundRect(50, 160, 100, 40, 30, 30);
+g.drawString("Ellips rechthoek", 55, 220);
+g.setColor(Color.MAGENTA);
+g.fillOval(175, 160, 100, 40);
+g.setColor(Color.BLACK);
+g.drawString("Gevulde ovaal", 185, 220);
+g.drawRoundRect(325, 160, 50, 45, 90, 90);
+g.drawString("Cirkel", 335, 225  );
 
 
 
